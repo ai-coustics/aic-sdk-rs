eac64f51d664e6a887494c7d48cb2ffc81b3cdce
Tobias <76444201+Fl1tzi@users.noreply.github.com>
2025-08-12 14:07:13 +0200

    Fix multiple definitions when importing C SDK (#1)
    * Fix multiple definitions when importing C SDK
    
    * Add documentation to build
    
    * curated suffix as seperate variable

--- aic-sdk-sys/build.rs
+++ aic-sdk-sys/build.rs
@@ -1,27 +1,88 @@
 use std::env;
 use std::path::PathBuf;
+use std::process::Command;
 
+#[cfg(not(target_os = "linux"))]
 fn main() {
-    // Get the directory of the current build script.
-    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    panic!("This platform is currently not supported.")
+}
 
-    // Construct the path to the 'libs' directory relative to the manifest directory.
-    let lib_dir = manifest_dir.join("libs");
+/// When compiling a static library, the Rust compiler includes
+/// non-exported symbols. The following code curates the symbols
+/// to only include aic_* symbols in the library.
+///
+/// Issue: https://github.com/rust-lang/rust/issues/104707
+#[cfg(target_os = "linux")]
+fn main() {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let lib_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("libs");
 
-    // Tell cargo where to find the native library.
-    println!("cargo:rustc-link-search=native={}", lib_dir.display());
+    let lib_name = "aic";
+    let lib_name_curated = "aic_curated";
+    let global_symbols_wildcard = "aic_*";
 
-    // Tell cargo to link against the 'aic' library statically.
-    // Cargo automatically handles the platform-specific library name (.a or .lib).
-    println!("cargo:rustc-link-lib=static=aic");
+    let static_lib = lib_path.join(format!("lib{}.a", lib_name));
 
-    // Tell cargo to rerun the build script if the library changes.
+    if !static_lib.exists() {
+        panic!("Please provide the SDK at {}", static_lib.display());
+    }
+
+    // original .o file
+    let intermediate_obj = out_dir.join(format!("lib{}.o", lib_name));
+
+    // modified .o file
+    let final_obj = out_dir.join(format!("lib{}.o", lib_name_curated));
+
+    // .a file
+    let final_lib = out_dir.join(format!("lib{}.a", lib_name_curated));
+
+    // partially link
+    let ld_status = Command::new("ld")
+        .arg("-r")
+        .arg("-o")
+        .arg(&intermediate_obj)
+        .arg("--whole-archive")
+        .arg(&static_lib)
+        .status()
+        .expect("Failed to execute ld command.");
+
+    if !ld_status.success() {
+        panic!("ld -r command failed for {}", static_lib.display());
+    }
+
+    // curate symbols (only keep specific symbols)
+    let objcopy_status = Command::new("objcopy")
+        .arg("--wildcard")
+        .arg("--keep-global-symbol")
+        .arg(global_symbols_wildcard)
+        .arg(&intermediate_obj)
+        .arg(&final_obj)
+        .status()
+        .expect("Failed to execute objcopy command.");
+
+    if !objcopy_status.success() {
+        panic!("objcopy command failed for {}", intermediate_obj.display());
+    }
+
+    // build the archive
+    let ar_status = Command::new("ar")
+        .arg("rcs")
+        .arg(&final_lib)
+        .arg(&final_obj)
+        .status()
+        .expect("Failed to execute ar.");
+
+    if !ar_status.success() {
+        panic!("objcopy command failed for {}", final_obj.display());
+    }
+
+    // link with the curated library
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
     println!(
-        "cargo:rerun-if-changed={}",
-        lib_dir.join("libaic.a").display()
+        "cargo:rustc-link-lib=static={}",
+        format!("{}", lib_name_curated)
     );
-    println!(
-        "cargo:rerun-if-changed={}",
-        lib_dir.join("aic.lib").display()
-    ); // For Windows
+
+    // Rerun this script if the static library changes.
+    println!("cargo:rerun-if-changed={}", static_lib.display());
 }
