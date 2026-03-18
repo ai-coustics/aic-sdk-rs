3d127ff4751a105d7bf535cf4136eee18bcc2dbe
Stephan Eckes <stephan@neodsp.com>
2026-03-18 18:00:26 +0100

    Update src/processor.rs
    Co-authored-by: Andres O. Vela <andresovela@users.noreply.github.com>

--- src/processor.rs
+++ src/processor.rs
@@ -107,7 +107,7 @@ pub enum ProcessorParameter {
     /// The exact behavior depends on the active model:
     /// - **Quail Models:** Controls how aggressively the model suppresses noise. When used
     ///   with Quail Voice Focus, it also suppresses background and competing speech.
-    /// - **Sparrow Models:** Controls the mixback and therefore the intensity of the
+    /// - **Rook Models:** Controls the mixback and therefore the intensity of the
     ///   enhancement.
     ///
     /// **Range:** 0.0 to 1.0
