a1bf227286e894c9d102597de71c106abdcdfb92
steckes <stephan@neodsp.com>
2026-05-19 16:17:01 +0200

    add batches for docs.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
 #![doc = include_str!("../README.md")]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 use aic_sdk_sys::{aic_get_compatible_model_version, aic_get_sdk_version, aic_set_sdk_wrapper_id};
 use std::ffi::CStr;
 
 #[cfg(feature = "download-model")]
+#[cfg_attr(docsrs, doc(cfg(feature = "download-model")))]
 mod download;
 mod error;
 mod model;
 mod processor;
 #[cfg(feature = "async")]
+#[cfg_attr(docsrs, doc(cfg(feature = "async")))]
 mod processor_async;
 mod vad;
 
@@ -16,6 +19,7 @@ pub use error::*;
 pub use model::*;
 pub use processor::*;
 #[cfg(feature = "async")]
+#[cfg_attr(docsrs, doc(cfg(feature = "async")))]
 pub use processor_async::*;
 pub use vad::*;
 
