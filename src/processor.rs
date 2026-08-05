a54d0189605d0ea5e3186b2ca12a70530c2dd4b0
andresovela <andresovela@gmail.com>
2026-08-05 16:15:05 +0200

    Add missing suggestions

--- src/processor.rs
+++ src/processor.rs
@@ -761,8 +761,8 @@ impl<'a> Processor<'a> {
     /// This function is meant to be used in lifecycle management events.
     /// A telemetry session is automatically stopped when a processor is destroyed.
     /// However, in cases where this SDK is integrated with languages with automatic memory
-    /// management, object deallocation could be delayed. Use this function to start
-    /// termination on demand.
+    /// management, object deallocation could be delayed. Use this function to terminate
+    /// the session explicitly.
     ///
     /// This function blocks until the telemetry session is terminated, unless another
     /// session is still alive. In that case, this function returns early and termination
--- src/vad.rs
+++ src/vad.rs
@@ -417,8 +417,8 @@ impl<'a> Vad<'a> {
     /// This function is meant to be used in lifecycle management events.
     /// A telemetry session is automatically stopped when a VAD is destroyed.
     /// However, in cases where this SDK is integrated with languages with automatic memory
-    /// management, object deallocation could be delayed. Use this function to start
-    /// termination on demand.
+    /// management, object deallocation could be delayed. Use this function to terminate
+    /// the session explicitly.
     ///
     /// This function blocks until the telemetry session is terminated, unless another
     /// session is still alive. In that case, this function returns early and termination
